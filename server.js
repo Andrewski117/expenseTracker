ed0b688882360ad7318b8e83df26d2211bfa549f
Andrew Skillett <andrewskillett@gmail.com>
2022-10-09 10:14:35 -0700

    added ejs rendering and post foundation to get expense

--- server.js
+++ server.js
@@ -3,10 +3,18 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.set('view engine', 'ejs');
+//app.use(express.static('public'));
+
 app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    //res.sendFile(__dirname + '/public/index.html')
+    res.render('index.ejs');
+})
+
+app.post('/expenses', (req, res) => {
+    console.log('submitted')
 })
 
 app.listen(PORT, () => {
-    console.log(`running on port: ${PORT}`)
+    console.log(`running on port billy: ${PORT}`)
 })
